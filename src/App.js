be9f8d4b2cd32d8d0357b3e75a6d81c832b69230
Ryan Howard <ryanghoward@protonmail.com>
2022-08-24 14:55:50 -0700

    Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-function App() {
+import Header from "./components/Header";
+import React from "react";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const App = () => {
   return (
-    <div className='App'>
-      <h1>Maison</h1>
+    <div className='max-w-[1440px] mx-auto bg-white'>
+      <BrowserRouter>
+        <Header />
+        {/* <Routes>
+          <Route path='/' element={<Header />} />
+        </Routes> */}
+      </BrowserRouter>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link } from "react-router-dom";
+// import logo from "../assets/MLogo.jpg";
+
+const Header = () => {
+  return (
+    <header className='py-6 mb-12 border-b'>
+      <div className='container mx-auto flex justify-between items-center'>
+        <Link to='/'>
+          {/* <img src={logo} alt='logo' /> */}
+          <h1>Maison</h1>
+        </Link>
+        <div className='flex items-center gap-6'>
+          <Link className='hover:text-violet-900 transition' to='/'>
+            Log In
+          </Link>
+          <Link
+            className='bg-violet-700 hover:bg-violet-800 text-white px-4 py-3 rounded-lg transition'
+            to='/'
+          >
+            Sign Up
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
