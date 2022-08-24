bb9f024fef36fbe1206267daff0db8f1fd2a9338
Ryan Howard <ryanghoward@protonmail.com>
2022-08-24 15:05:00 -0700

    Banner

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import Header from "./components/Header";
 import React from "react";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 const App = () => {
   return (
     <div className='max-w-[1440px] mx-auto bg-white'>
       <BrowserRouter>
         <Header />
-        {/* <Routes>
-          <Route path='/' element={<Header />} />
-        </Routes> */}
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
       </BrowserRouter>
     </div>
   );
